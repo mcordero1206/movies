805f4881b6b4cb415d9c95581808a34913d18f51
Michael Cordero <mcordero1206@gmail.com>
2017-09-27 11:27:52 -0400

    Update fresh_tomatoes.py

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -11,7 +11,7 @@ main_page_head = '''
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title>Chance's Favorite Movies</title>
+    <title>Mike's Favorite Movies</title>
     <!-- Bootstrap 3 -->
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap-theme.min.css">
@@ -111,7 +111,7 @@ main_page_content = '''
       <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <a class="navbar-brand" href="#">Chance's Favorite Movie Trailers</a>
+            <a class="navbar-brand" href="#">Mike's Favorite Movie Trailers</a>
           </div>
         </div>
       </div>
